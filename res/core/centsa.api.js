93dcabdd7f8c6f8b866e66c0310ca26f931ba9d4
Muhammed Haque <muhammed.haque@bond.co.uk>
2017-09-25 17:31:55 +0100

    Fixed JavaScript and scoping

--- res/core/centsa.api.js
+++ res/core/centsa.api.js
@@ -1,35 +1,35 @@
 /**
  * 
  */
+var centsa = (function() {
+	var key = null;
 
-// make simple async http request
-function ajax(payload) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open(payload.method, payload.url,
-			payload.async != undefined ? payload.async : true);
-	xmlHttp.onreadystatechange = function() {
-		if (this.readyState == 4) {
-			if (payload.complete) {
-				payload.complete(this);
-			}
-			if (this.status >= 200 && this.status <= 300) {
-				if (payload.success) {
-					payload.success(this);
+	// make simple async http request
+	function ajax(payload) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open(payload.method, payload.url,
+				payload.async != undefined ? payload.async : true);
+		xmlHttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				if (payload.complete) {
+					payload.complete(this);
 				}
-			} else {
-				if (payload.error) {
-					payload.error(this);
+				if (this.status >= 200 && this.status <= 300) {
+					if (payload.success) {
+						payload.success(this);
+					}
+				} else {
+					if (payload.error) {
+						payload.error(this);
+					}
 				}
 			}
 		}
+		xmlHttp.send(payload.data);
 	}
-	xmlHttp.send(payload.data);
-}
 
-var centsa = new (function() {
-	var key = null;
 	return {
-		setUniqueKey : function(ukey) 
+		setUniqueKey : function(ukey) {
 			key = ukey;
 		},
 		settings : {
