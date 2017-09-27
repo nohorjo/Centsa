ff2c0a14e46d26210385c88b2d1f20574de0cd96
Muhammed Haque <muhammed.haque@bond.co.uk>
2017-09-27 14:17:37 +0100

    Created DAO interface

---
+++ src/nohorjo/centsa/dbservices/DAOI.java
@@ -0,0 +1,7 @@
+package nohorjo.centsa.dbservices;
+
+import java.sql.SQLException;
+
+public interface DAOI {
+	public void createTable() throws SQLException;
+}
--- src/nohorjo/centsa/dbservices/TransactionDAO.java
+++ src/nohorjo/centsa/dbservices/TransactionDAO.java
@@ -4,14 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class TransactionDAO {
+public class TransactionDAO implements DAOI {
 
 	static {
+		try {
+			new TransactionDAO().createTable();
+		} catch (SQLException e) {
+			throw new Error(e);
+		}
+	}
+
+	@Override
+	public void createTable() throws SQLException {
 		try (Connection conn = SQLUtils.getConnection();
 				PreparedStatement ps = conn.prepareStatement(SQLUtils.getQuery("Transaction.CreateTable"))) {
 			ps.execute();
-		} catch (SQLException e) {
-			throw new Error(e);
 		}
 	}
 }
