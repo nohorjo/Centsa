072f210a1d8bb40d7d9b907a588bbb9dd33f1db4
Muhammed Haque <muhammed.haque@bond.co.uk>
2017-09-29 16:13:47 +0100

    Changed to use servlets

--- src/nohorjo/centsa/rest/SettingsRS.java
+++ src/nohorjo/centsa/rest/SettingsRS.java
@@ -4,32 +4,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import nohorjo.centsa.properties.SystemProperties;
-import nohorjo.centsa.server.HttpRequestProcessor;
 import nohorjo.centsa.vo.SettingsVO;
 
-public class SettingsRS implements HttpRequestProcessor {
+public class SettingsRS extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4610961678167873846L;
 
 	@Override
-	public void process(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		switch (req.getMethod()) {
-		case "POST":
-			saveSetting(req, resp);
-			break;
-		case "GET":
-			getSetting(req, resp);
-			break;
-		default:
-			resp.sendError(405, "Method not allowed");
-		}
-	}
-
-	private void getSetting(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		int status = 404;
 		String value = SystemProperties.get(req.getParameter("key"), Object.class).toString();
 		if (value != null) {
@@ -41,7 +33,8 @@ public class SettingsRS implements HttpRequestProcessor {
 		resp.setStatus(status);
 	}
 
-	private void saveSetting(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		SettingsVO vo = new ObjectMapper().readValue(
 				req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), SettingsVO.class);
 		SystemProperties.set(vo.getKey(), vo.getValue());
--- src/nohorjo/centsa/server/APIRequestHandler.java
+++ src/nohorjo/centsa/server/APIRequestHandler.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,41 +20,36 @@ public class APIRequestHandler {
 
 	public static final String UNIQUE_KEY = UUID.randomUUID().toString();
 
-	private static final Map<String, HttpRequestProcessor> PROCESSORS = new HashMap<>();
+	private static final Map<String, HttpServlet> SERVLETS = new HashMap<>();
 	private static final Logger log = LoggerFactory.getLogger(APIRequestHandler.class);
 
 	static {
 		log.info("Key set to {}", UNIQUE_KEY);
-		PROCESSORS.put("settings", new SettingsRS());
+		SERVLETS.put("settings", new SettingsRS());
 	}
 
 	public static void handle(HttpServletRequest request, HttpServletResponse response, String target)
-			throws IOException {
-		HttpRequestProcessor processor;
+			throws IOException, ServletException {
 
 		String key = target.split("/")[0];
 		String _target = target.replaceAll("^[^/]*/", "");
 		if (key.equals(UNIQUE_KEY)) {
-			processor = PROCESSORS.get(_target);
+			HttpServlet servlet = SERVLETS.get(_target);
 
-			if (processor == null) {
-				processor = (HttpServletRequest req, HttpServletResponse resp) -> {
-					resp.setStatus(404);
-					try (PrintWriter writer = resp.getWriter()) {
-						writer.write("Endpoint not found: " + _target);
-					}
-				};
+			if (servlet != null) {
+				servlet.service(request, response);
+			} else {
+				response.setStatus(404);
+				try (PrintWriter writer = response.getWriter()) {
+					writer.write("Endpoint not found: " + _target);
+				}
 			}
 		} else {
-			processor = (HttpServletRequest req, HttpServletResponse resp) -> {
-				resp.setStatus(401);
-				try (PrintWriter writer = resp.getWriter()) {
-					writer.write("Invalid key: " + key);
-				}
-			};
+			response.setStatus(401);
+			try (PrintWriter writer = response.getWriter()) {
+				writer.write("Invalid key: " + key);
+			}
 		}
-
-		processor.process(request, response);
 	}
 
 }
