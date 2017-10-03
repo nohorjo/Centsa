eb31751f2921d9c48282466a620f9cca980fd439
nohorjo <nohorjo@gmail.com>
2017-10-03 22:49:07 +0100

    Created templates for accounts, types and expenses

--- ui/default/routes.js
+++ ui/default/routes.js
@@ -1,8 +1,17 @@
 app.config(function($routeProvider) {
-	$routeProvider.when("/transaction", {
-		templateUrl : "transaction/view.html",
+	$routeProvider.when("/transactions", {
+		templateUrl : "transactions/view.html",
 		controller : "transCtrl"
+	}).when("/accounts", {
+		templateUrl : "accounts/view.html",
+		controller : "accountsCtrl"
+	}).when("/types", {
+		templateUrl : "types/view.html",
+		controller : "typesCtrl"
+	}).when("/expenses", {
+		templateUrl : "expenses/view.html",
+		controller : "expensesCtrl"
 	}).otherwise({
-		redirectTo : '/transaction'
+		redirectTo : '/transactions'
 	});
 });
