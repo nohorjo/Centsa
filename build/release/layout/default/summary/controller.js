c6d4480cf71b345b6a15a7fd1bbc31ae17bfb919
nohorjo <nohorjo@gmail.com>
2017-10-14 23:11:47 +0100

    Added more information

--- build/release/layout/default/summary/controller.js
+++ build/release/layout/default/summary/controller.js
@@ -1,9 +1,10 @@
 app.controller("summaryCtrl", function($scope) {
 	$scope.budget = centsa.general.budget();
 	$scope.rules = centsa.general.rules();
-	$scope.rule = $scope.rules[0];
+	$scope.rule = "default";
 
 	$scope.importFile = function() {
+		alert("Importing may take a while");
 		centsa.general.importFile($scope.rule);
 	}
 });
--- build/release/rules/default.js
+++ build/release/rules/default.js
@@ -1,3 +1,12 @@
+/**
+ * 	Script is initialized with the variables:
+ * 		$records : an iterator of the records in the CSV
+ * 		$transaction , $accounts , $types , $expenses : handlers
+ * 
+ * 	The handlers each expose the function ' insert(json) ' which creates
+ * 	the entity (if it doesn't exist) and returns its ID.
+ */
+
 var sdf = new java.text.SimpleDateFormat("yyyy/MM/dd");
 
 $records.next();// skip header
