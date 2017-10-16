0a2494fc77ab0f9aec5142e2ce2e8145b223b3cd
nohorjo <nohorjo@gmail.com>
2017-10-16 08:38:20 +0100

    Addded support for png images. Added logo

--- src/nohorjo/centsa/rest/core/LayoutRS.java
+++ src/nohorjo/centsa/rest/core/LayoutRS.java
@@ -1,15 +1,19 @@
 package nohorjo.centsa.rest.core;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import nohorjo.centsa.properties.SystemProperties;
 
-@Path("/")
+@Path("/layout")
 public class LayoutRS {
 
 	@GET
@@ -20,10 +24,29 @@ public class LayoutRS {
 				.getSystemResourceAsStream("layout/" + SystemProperties.get("layout", String.class) + "/" + resource)) {
 			int b;
 			while ((b = in.read()) != -1) {
-				writer.write(b);
+				writer.write((byte) b);
 			}
 		}
 		return writer.toString();
+	}
+
+	@GET
+	@Path("/{resource:.*\\.png}")
+	@Produces("image/png")
+	public byte[] getImage(@PathParam("resource") String resource) throws IOException {
+		List<Byte> bs = new LinkedList<>();
+		try (InputStream in = ClassLoader
+				.getSystemResourceAsStream("layout/" + SystemProperties.get("layout", String.class) + "/" + resource)) {
+			int b;
+			while ((b = in.read()) != -1) {
+				bs.add((byte) b);
+			}
+		}
+		byte[] rtn = new byte[bs.size()];
+		for (int i = 0; i < rtn.length; i++) {
+			rtn[i] = bs.get(i);
+		}
+		return rtn;
 	}
 
 }
