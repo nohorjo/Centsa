993302d8ea75e41542eb5a0a6cf3c489a12aed9d
Muhammed Haque <nohorjo@gmail.com>
2017-12-22 20:22:55 +0000

    Prepared setup for Scala

--- src/main/nohorjo/centsa/Main.java
+++ src/main/nohorjo/centsa/Main.java
@@ -9,37 +9,36 @@ import nohorjo.centsa.updater.UpdateChecker;
 
 /**
  * Main class of the application
- * 
- * @author muhammed.haque
  *
+ * @author muhammed.haque
  */
 public class Main extends Application {
 
-	private static Stage stage;
+    private static Stage stage;
 
-	/**
-	 * Gets the stage
-	 * 
-	 * @return The stage
-	 */
-	public static Stage getStage() {
-		return stage;
-	}
+    /**
+     * Gets the stage
+     *
+     * @return The stage
+     */
+    public static Stage getStage() {
+        return stage;
+    }
 
-	@Override
-	public void start(Stage stage) {
-		Main.stage = stage;
-		stage.setScene(new Scene(new Renderer(stage)));
-	}
+    @Override
+    public void start(Stage stage) {
+        Main.stage = stage;
+        stage.setScene(new Scene(new Renderer(stage)));
+    }
 
-	@Override
-	public void stop() throws Exception {
-		EmbeddedServer.stopServer();
-	}
+    @Override
+    public void stop() throws Exception {
+        EmbeddedServer.stopServer();
+    }
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         UpdateChecker.initialise();
-		EmbeddedServer.startServer();
-		launch(args);
-	}
+        EmbeddedServer.startServer();
+        launch(args);
+    }
 }
