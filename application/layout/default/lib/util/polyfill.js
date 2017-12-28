866d9a7ce49b06c246ea595ec70c029a04e786ab
Muhammed Haque <muhammed.haque@bond.co.uk>
2017-12-28 14:18:43 +0000

    Improved startsWith polyfill

--- application/layout/default/lib/util/polyfill.js
+++ application/layout/default/lib/util/polyfill.js
@@ -11,12 +11,5 @@ Object.assign = Object.assign || function assign(base, toCopy) {
 };
 
 String.prototype.startsWith = String.prototype.startsWith || function(s) {
-	if (s) {
-		for (var i = 0; i < s.length; i++) {
-			if (this[i] != s[i])
-				return false;
-		}
-		return true;
-	}
-	return false;
+	return this.substr(0, s.length) === s
 };
