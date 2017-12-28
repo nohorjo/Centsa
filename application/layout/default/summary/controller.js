7d807c7284b0de8ec29476ce432b3082dcd97de6
Muhammed Haque <nohorjo@gmail.com>
2017-12-28 20:24:50 +0000

    Added zooming to chart

--- application/layout/default/summary/controller.js
+++ application/layout/default/summary/controller.js
@@ -1,69 +1,84 @@
 app.controller("summaryCtrl", function($scope) {
-	$scope.strictMode = centsa.settings.get("strict.mode") == "true";
+    $scope.strictMode = centsa.settings.get("strict.mode") == "true";
 
-	$scope.getBudget = function() {
-		centsa.settings.set("strict.mode", $scope.strictMode);
-		$scope.budget = centsa.general.budget($scope.strictMode);
-	};
+    $scope.getBudget = function() {
+        centsa.settings.set("strict.mode", $scope.strictMode);
+        $scope.budget = centsa.general.budget($scope.strictMode);
+    };
 
-	$scope.getBudget();
+    $scope.getBudget();
 
-	(function() {
-		var sums = []
-		var data = centsa.transactions.getCumulativeSums();
-		$(data).each(function() {
-			sums.push({
-				date : new Date(this.date).formatDate("yyyy/MM/dd"),
-				sum : this.sum / 100
-			});
-		});
-		var valueAxes = [ {
-			"id" : "v1",
-			"axisAlpha" : 0,
-			"position" : "left",
-			"ignoreAxisWidth" : true
-		} ];
-		var graphs = [ {
-			"id" : "g1",
-			"balloon" : {
-				"drop" : true,
-				"adjustBorderColor" : false,
-				"color" : "#ffffff"
-			},
-			"lineThickness" : 2,
-			"useLineColorForBulletBorder" : true,
-			"valueField" : "sum",
-			"balloonText" : "<span>[[value]]</span>"
-		} ];
-		var chartOpts = {
-			"type" : "serial",
-			"theme" : "light",
-			"marginRight" : 60,
-			"marginLeft" : 60,
-			"dataDateFormat" : "YYYY/MM/DD",
-			"valueAxes" : valueAxes,
-			"balloon" : {
-				"borderThickness" : 1,
-				"shadowAlpha" : 0
-			},
-			"graphs" : graphs,
-			"chartCursor" : {
-				"pan" : true,
-				"valueLineEnabled" : true,
-				"valueLineBalloonEnabled" : true,
-				"cursorAlpha" : 1,
-				"cursorColor" : "#258cbb",
-				"limitToGraph" : "g1",
-				"valueLineAlpha" : 0.2,
-				"valueZoomable" : true
-			},
-			"categoryField" : "date",
-			"categoryAxis" : {
-				"parseDates" : false,
-				"dashLength" : 1,
-			},
-			"dataProvider" : sums
-		};
-		AmCharts.makeChart("trans-chart", chartOpts);
-	})();
+    (function() {
+        var sums = []
+        var data = centsa.transactions.getCumulativeSums();
+        $(data).each(function() {
+            sums.push({
+                date : new Date(this.date).formatDate("yyyy/MM/dd"),
+                sum : this.sum / 100
+            });
+        });
+        var valueAxes = [ {
+            "id" : "v1",
+            "axisAlpha" : 0,
+            "position" : "left",
+            "ignoreAxisWidth" : true
+        } ];
+        var graphs = [ {
+            "id" : "g1",
+            "balloon" : {
+                "drop" : true,
+                "adjustBorderColor" : false,
+                "color" : "#ffffff"
+            },
+            "lineThickness" : 2,
+            "useLineColorForBulletBorder" : true,
+            "valueField" : "sum",
+            "balloonText" : "<span>[[value]]</span>"
+        } ];
+        var chartOpts = {
+            "type" : "serial",
+            "theme" : "light",
+            "marginRight" : 60,
+            "marginLeft" : 60,
+            "dataDateFormat" : "YYYY/MM/DD",
+            "valueAxes" : valueAxes,
+            "balloon" : {
+                "borderThickness" : 1,
+                "shadowAlpha" : 0
+            },
+            "graphs" : graphs,
+            "chartCursor" : {
+                "pan" : true,
+                "valueLineEnabled" : true,
+                "valueLineBalloonEnabled" : true,
+                "cursorAlpha" : 1,
+                "cursorColor" : "#258cbb",
+                "limitToGraph" : "g1",
+                "valueLineAlpha" : 0.2,
+                "valueZoomable" : true
+            },
+            "categoryField" : "date",
+            "categoryAxis" : {
+                "parseDates" : false,
+                "dashLength" : 1,
+            },
+            "chartScrollbar": {
+                "graph": "g1",
+                "oppositeAxis":false,
+                "offset":30,
+                "scrollbarHeight": 80,
+                "backgroundAlpha": 0,
+                "selectedBackgroundAlpha": 0.1,
+                "selectedBackgroundColor": "#888888",
+                "graphFillAlpha": 0,
+                "graphLineAlpha": 0.5,
+                "selectedGraphFillAlpha": 0,
+                "selectedGraphLineAlpha": 1,
+                "color":"#AAAAAA"
+            },
+            "dataProvider" : sums,
+            "mouseWheelZoomEnabled":true
+        };
+        AmCharts.makeChart("trans-chart", chartOpts);
+    })();
 });
