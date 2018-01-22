146183accda8fc09b7c5bdf1058d9d73de16dd29
Muhammed Haque <nohorjo@gmail.com>
2018-01-22 20:40:42 +0000

    #85

--- readme.md
+++ readme.md
@@ -4,11 +4,12 @@ Centsa is a simple personal budgeting application to help you manage your money.
 
 [Check out the Wiki for more information](https://github.com/nohorjo/Centsa/wiki)
 
-## Installing
-Download and install [the latest binaries](https://github.com/nohorjo/Centsa/releases) for your system.
+## Installing and running
+```bash
+git clone https://github.com/nohorjo/Centsa.git
+cd Centsa
+npm install
+npm start
+```
 
 Changelog can be found [here](https://github.com/nohorjo/Centsa/wiki/Changelog)
-
-## Building
-This application is built using [electron-forge](https://electronforge.io).  
-To build run `npm make` in the root directory.
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,9 @@
-import { app, BrowserWindow, dialog } from 'electron';
-import * as fs from 'fs-extra';
+import * as express from 'express';
 
-import Updater from './Updater';
-import Expenses from './Expenses';
-import { settings } from './Settings';
+let port = process.env.PORT || 8080;
 
-let mainWindow: Electron.BrowserWindow | null = null;
+const app = express();
 
-let _init = () => init((options: Electron.BrowserWindowConstructorOptions) => new BrowserWindow(options));
+app.get('/', (req,res)=>res.send('Loading'));
 
-app.on('ready', _init);
-app.on('activate', _init);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-export default function init(
-  newBrowserWindow: (options: Electron.BrowserWindowConstructorOptions) => Electron.BrowserWindow
-) {
-  try {
-    if (mainWindow === null) {
-      const centsaDir = `${app.getPath("home")}/.centsa`;
-      try {
-        fs.mkdirSync(centsaDir);
-        fs.copySync(`${__dirname}/../assets/defaultsettings.json`, `${centsaDir}/settings.json`);
-        // TODO: initialize new setup
-      } catch (error) {
-        if (error.code != 'EEXIST') throw error;
-      }
-
-      settings.init(centsaDir);
-
-      Updater.autoCheckUpdate((update,confirm) => {
-          dialog.showMessageBox({
-            type: "question",
-            title: "New update available!",
-            message: `Centsa ${update.version} is out. Would you like to update?`,
-            detail: update.changelog,
-            buttons: ["Yes", "No"]
-          }, r => {
-            if (r == 0) {
-              confirm();
-            }
-          });
-      });
-      Expenses.applyAutoTrans(() => {
-        if (mainWindow) { mainWindow.loadURL(`file://${__dirname}/main.html`); }
-      });
-
-      mainWindow = newBrowserWindow({
-        icon: `${__dirname}/../assets/icon.png`,
-        width: settings.width,
-        height: settings.height,
-      });
-
-
-      mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-      mainWindow.on('closed', () => {
-        mainWindow = null;
-      });
-
-      let saveSize = () => {
-        settings.width = mainWindow!.getSize()[0];
-        settings.height = mainWindow!.getSize()[1];
-        settings.save();
-      };
-
-      mainWindow.on("maximize", saveSize);
-      mainWindow.on("unmaximize", saveSize);
-      mainWindow.on("resize", saveSize);
-    }
-  } catch (error) {
-    console.error(error);
-    dialog.showErrorBox("Error", JSON.stringify(error));
-    app.quit();
-  }
-}
+app.listen(port);
