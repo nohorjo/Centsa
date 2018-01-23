d30d7f7d468f554b92c133187264c99330e69451
Muhammed Haque <nohorjo@gmail.com>
2018-01-23 08:08:35 +0000

    #85 started on login

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ let port = process.env.PORT || 8080;
 
 const app = express();
 
-app.get(['/', ''], (req,res)=>res.send('Loading'));
+app.get(['/', ''], (req, res) => res.redirect('/index.html'));
 
 app.use(express.static('static'));
 
-app.listen(port);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
