4e66ae7c3443b3fa16cd6ded6829d432ffecbc5c
Muhammed Haque <nohorjo@gmail.com>
2018-01-31 17:50:32 +0000

    #85 fb login continue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import axios from 'axios';
 
 let port = process.env.PORT || 8080;
 
 const app = express();
 
-app.get(['/', ''], (req, res) => res.redirect('/index.html'));
-
 app.use(express.static('static'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get(['/', ''], (req, res) => res.redirect('/index.html'));
+app.post('/fb', (req, res) => {
+    console.dir(req.body);
+    let fields = ['email', 'name'];
+    try {
+        let url = `https://graph.facebook.com/${req.body.userID}?access_token=${req.body.accessToken}&fields=${fields.join(',')}`;
+
+        axios.get(url).then((resp) => {
+            console.dir(resp.data);
+            res.send("done");
+        }).catch(()=>{
+            res.send("error");
+        });
+    } catch (e) { }
+});
+
+
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
---
+++ static/facebook.js
@@ -0,0 +1,36 @@
+let fbInit = () => {
+
+    window.checkLoginState = function () {
+        FB.getLoginStatus(function (response) {
+            console.dir(response.authResponse);
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/fb');
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(response.authResponse));
+        });
+    };
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '531919070519328',
+            cookie: true,
+            xfbml: true,
+            version: 'v2.11'
+        });
+
+        FB.AppEvents.logPageView();
+        FB.login(function (response) {
+            window.checkLoginState();
+        }, { scope: 'public_profile,email' });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11&appId=531919070519328&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
+}
