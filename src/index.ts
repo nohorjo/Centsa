4430e85f429ef8dd361988b0cf365948d509e05e
Muhammed Haque <nohorjo@gmail.com>
2018-02-08 07:47:03 +0000

    #85 Added clustering

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,56 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as session from 'express-session';
+import * as cluster from 'cluster';
+import * as os from 'os';
 import axios from 'axios';
 
-let port = process.env.PORT || 8080;
+if (cluster.isMaster) {
+    for (var i = 0; i < os.cpus().length; i += 1) {
+        cluster.fork();
+    }
+} else {
+    const port = process.env.PORT || 8080;
 
-const app = express();
+    const app = express();
 
-const sess = {
-    // genid: function(req) {
-    //     return genuuid() // use UUIDs for session IDs
-    //   },
-    secret: process.env.SECRET || Math.random().toString(),//FIXME:
-    cookie: {}
+    const sess = {
+        secret: process.env.SECRET || Math.random().toString(),//FIXME:
+        resave: false,
+        saveUninitialized: false,
+        cookie: {}
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        app.set('trust proxy', 1);
+        sess.cookie['secure'] = true;
+    }
+
+    app.use(session(sess));
+
+    app.all('/app/*', (req, resp, next) => (req.session && req.session.authorized) ? next() : resp.status(401).send());
+
+    app.use(express.static('static'));
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+
+
+    app.get(['/', ''], (req, res) => res.redirect('/index.html'));
+    app.post('/fb', (req, res) => {
+        let fields = ['email', 'name'];
+        try {
+            let url = `https://graph.facebook.com/${req.body.userID}?access_token=${req.body.accessToken}&fields=${fields.join(',')}`;
+
+            axios.get(url).then((resp) => {
+                console.dir(resp.data);
+                res.send("done");
+            }).catch(e => {
+                res.status(500).send(e);
+            });
+        } catch (e) { }
+    });
+
+    app.listen(port, () => console.log(`Server ${cluster.worker.id} listening on port ${port}`));
 }
-
-if (process.env.NODE_ENV === 'production') {
-    app.set('trust proxy', 1);
-    sess.cookie['secure'] = true;
-}
-
-app.use(session(sess));
-
-app.all('/app/*', (req, resp, next) => (req.session && req.session.authorized) ? next() : resp.status(401).send());
-
-app.use(express.static('static'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-
-app.get(['/', ''], (req, res) => res.redirect('/index.html'));
-app.post('/fb', (req, res) => {
-    let fields = ['email', 'name'];
-    try {
-        let url = `https://graph.facebook.com/${req.body.userID}?access_token=${req.body.accessToken}&fields=${fields.join(',')}`;
-
-        axios.get(url).then((resp) => {
-            console.dir(resp.data);
-            res.send("done");
-        }).catch(e => {
-            res.status(500).send(e);
-        });
-    } catch (e) { }
-});
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-
