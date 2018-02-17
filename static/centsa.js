4b71cd6e1206dfd06ee72ea6584c38597d88c8f9
Muhammed Haque <nohorjo@gmail.com>
2018-02-17 10:25:17 +0000

    #85 Updated expenses controller

--- static/centsa.js
+++ static/centsa.js
@@ -1,13 +1,18 @@
 app.service('centsa'), function ($http) {
-    function genericApi(path) {
-        const apiUrl = `/api/${path}`;
-
-        this.getAll = (success, error) => $http.get(apiUrl).then(resp => success(resp.data), error);
-        this.insert = (item, success, error) => $http.post(apiUrl, item).then(resp => success(resp.data), error);
-        this.remove = (id, success, error) => $http.delete(`${apiUrl}/${id}`, success, error);
-        
+    class genericApi {
+        constructor(path) { this.apiUrl = `/api/${path}`; }
+        getAll(success, error) { $http.get(this.apiUrl).then(resp => success(resp.data), error); }
+        insert(item, success, error) { $http.post(this.apiUrl, item).then(resp => success(resp.data), error); }
+        remove(id, success, error) { $http.delete(`${this.apiUrl}/${id}`, success, error); }
     }
+    class expensesApi extends genericApi {
+        constructor() { super('expenses'); }
+        totalActive(incAuto, success, error) { $http.get(`${this.apiUrl}/total&auto=${incAuto}`).then(resp => success(resp.data), error); }
+    }
+
     this.accounts = new genericApi('accounts');
     this.types = new genericApi('types');
     this.transactions = new genericApi('transactions');
+    this.expenses = new expensesApi();
+
 };
--- static/expenses/controller.js
+++ static/expenses/controller.js
@@ -1,70 +1,66 @@
-app.controller("expensesCtrl", function($scope, $rootScope, $sce) {
+app.controller("expensesCtrl", ($scope, $rootScope, $sce) => {
     $scope.trust = $sce.trustAsHtml;
-    $scope.expenses = centsa.expenses.getAll(0, 0, "NAME ASC");
-    $scope.accounts = centsa.accounts.getAll(0, 0);
-    $scope.types = centsa.types.getAll(0, 0);
+    centsa.expenses.getAll(data => $scope.expenses = data);
+    centsa.accounts.getAll(data => $scope.accounts = data);
+    centsa.types.getAll(data => $scope.types = data);
 
     $scope.frequency = {
-        type : "basic",
-        last : false,
-        date : 0,
-        day : "MO",
-        month : "0"
+        type: "basic",
+        last: false,
+        date: 0,
+        day: "MO",
+        month: "0"
     };
 
-    
-    function getActiveTotals() {
-        $scope.totalActiveExpenses = centsa.expenses.totalActive();
-        $scope.totalAutoExpenses = centsa.expenses.totalActive(true);
+
+    const getActiveTotals = () => {
+        centsa.expenses.totalActive(false, data => $scope.totalActiveExpenses = data);
+        centsa.expenses.totalActive(true, data => $scope.totalAutoExpenses = data);
     }
-    
+
     getActiveTotals();
 
     $scope.newExpense = {
-        name : "",
-        cost : 0.0,
-        frequency : "",
-        started : new Date().formatDate("yyyy/MM/dd"),
-        automatic : false,
-        account_id : "",
-        type_id : "1"
+        name: "",
+        cost: 0.0,
+        frequency: "",
+        started: new Date().formatDate("yyyy/MM/dd"),
+        automatic: false,
+        account_id: "",
+        type_id: "1"
     };
-    var newExpense = Object.assign({}, $scope.newExpense);
+    let newExpense = Object.assign({}, $scope.newExpense);
 
-    $scope.saveExpense = function(updating) {
+    $scope.saveExpense = updating => {
         $scope.newExpense.cost = Math.round($scope.newExpense.cost * 100);
         $scope.newExpense.started = new Date($scope.newExpense.started).getTime();
-        if(!$scope.newExpense.account_id) {
+        if (!$scope.newExpense.account_id) {
             delete $scope.newExpense.account_id;
         }
-        var newId = centsa.expenses.insert($scope.newExpense);
-        if (updating) {
-            for (var i = 0; i < $scope.expenses.length; i++) {
-                if ($scope.expenses[i].id == $scope.newExpense.id) {
-                    $scope.expenses[i] = $scope.newExpense;
-                }
+        centsa.expenses.insert($scope.newExpense, newId => {
+            if (updating) {
+                $scope.expenses[$scope.expenses.findIndex(e => e.id == $scope.newExpense.id)] = $scope.newExpense;
+            } else {
+                $scope.newExpense.id = newId;
+                $scope.expenses.unshift($scope.newExpense);
             }
-        } else {
-            $scope.newExpense.id = newId;
-            $scope.expenses.unshift($scope.newExpense);
-        }
-        getActiveTotals();
-        $scope.newExpense = Object.assign({}, newExpense);
-        $('.datepicker[data-ng-model="newExpense.started"]').datepicker("update", new Date().formatDate("yyyy/MM/dd"));
+            getActiveTotals();
+            $scope.newExpense = Object.assign({}, newExpense);
+            $('.datepicker[data-ng-model="newExpense.started"]').datepicker("update", new Date().formatDate("yyyy/MM/dd"));
+        });
     };
 
-    $scope.getNow = function() {
-        return Date.now();
-    };
+    $scope.getNow = () => Date.now();
 
-    $scope.deleteExpense = function(id) {
+
+    $scope.deleteExpense = function (id) {
         if (centsa.expenses.remove(id)) {
             $scope.expenses = centsa.expenses.getAll(0, 0, "NAME ASC");
             getActiveTotals();
         }
     };
 
-    $scope.getMaxDaysInMonth = function() {
+    $scope.getMaxDaysInMonth = () => {
         switch ($scope.frequency.month) {
             case '0':
             case '1':
@@ -85,37 +81,35 @@ app.controller("expensesCtrl", function($scope, $rootScope, $sce) {
         }
     };
 
-    $scope.calculateNumberSuperscript = function(n) {
+    $scope.calculateNumberSuperscript = n => {
         n = Math.abs(n);
-        if(n > 10 && n < 20) {
+        if (n > 10 && n < 20) {
             return "th";
         }
         switch (n % 10) {
-        case 1: return "st";
-        case 2: return "nd";
-        case 3: return "rd";
-        default: return "th"
+            case 1: return "st";
+            case 2: return "nd";
+            case 3: return "rd";
+            default: return "th"
         }
     };
 
-    $scope.getDateMax = (function(){
-        var max;
-        return function(newMax, apply) {
-            return apply ? max = newMax : max;
-        }
+    $scope.getDateMax = (() => {
+        let max;
+        return (newMax, apply) => apply ? max = newMax : max;
     })();
 
-    $scope.frequencyDisplayText = function(freq) {
-        if(!freq) return;
-        var text;
-        if(/^\d+$/.test(freq)) {
+    $scope.frequencyDisplayText = freq => {
+        if (!freq) return;
+        let text;
+        if (/^\d+$/.test(freq)) {
             text = "Every " + freq + " day" + (freq == 1 ? "" : "s");
         } else {
             text = "On the ";
-            if(/^DATE \d+(\/\d+)?$/.test(freq)) {
+            if (/^DATE \d+(\/\d+)?$/.test(freq)) {
                 var date = freq.split(" ")[1];
 
-                if(date.indexOf("/") != -1) {
+                if (date.indexOf("/") != -1) {
                     date = date.split("/");
                     text += date[0] + "<sup>" + $scope.calculateNumberSuperscript(date[0])
                         + "</sup> of the " + date[1] + "<sup>"
@@ -123,27 +117,27 @@ app.controller("expensesCtrl", function($scope, $rootScope, $sce) {
                 } else {
                     text += date + "<sup>" + $scope.calculateNumberSuperscript(date) + "</sup>";
                 }
-            } else if(/^DAY -?\d+$/.test(freq)) {
+            } else if (/^DAY -?\d+$/.test(freq)) {
                 freq = freq.split(" ")[1];
                 text += Math.abs(freq) + "<sup>" + $scope.calculateNumberSuperscript(freq) + "</sup>"
                     + (freq < 0 ? " last " : " ") + "day of the month";
-            } else if(/^DAY (MO|TU|WE|TH|FR|SA|SU) -?\d+$/.test(freq)) {
-                var day = (function(d) {
-                    switch(d) {
-                    case "MO": return "Monday";
-                    case "TU": return "Tuesday";
-                    case "WE": return "Wednesday";
-                    case "TH": return "Thursday";
-                    case "FR": return "Friday";
-                    case "SA": return "Saturday";
-                    case "SU": return "Sunday";
+            } else if (/^DAY (MO|TU|WE|TH|FR|SA|SU) -?\d+$/.test(freq)) {
+                var day = (d => {
+                    switch (d) {
+                        case "MO": return "Monday";
+                        case "TU": return "Tuesday";
+                        case "WE": return "Wednesday";
+                        case "TH": return "Thursday";
+                        case "FR": return "Friday";
+                        case "SA": return "Saturday";
+                        case "SU": return "Sunday";
                     }
                 })(freq.substr(4, 2));
                 freq = freq.substr(7);
                 text += Math.abs(freq) + "<sup>" + $scope.calculateNumberSuperscript(freq) + "</sup>"
                     + (freq < 0 ? " last " : " ") + day + " of the month";
-            } else if(/^[RW]DAY -?\d+$/.test(freq)) {
-                var type = freq.charAt(0) == "R" ? "rest" : "working";
+            } else if (/^[RW]DAY -?\d+$/.test(freq)) {
+                const type = freq.charAt(0) == "R" ? "rest" : "working";
                 freq = freq.substr(5);
                 text += Math.abs(freq) + "<sup>" + $scope.calculateNumberSuperscript(freq) + "</sup>"
                     + (freq < 0 ? " last " : " ") + type + "-day of the month";
@@ -154,21 +148,18 @@ app.controller("expensesCtrl", function($scope, $rootScope, $sce) {
 
 
     $('.datepicker').datepicker({
-        format : "yyyy/mm/dd",
-        todayBtn : "linked",
-        autoclose : true,
-        todayHighlight : true
+        format: "yyyy/mm/dd",
+        todayBtn: "linked",
+        autoclose: true,
+        todayHighlight: true
     });
     $('.datepicker[data-ng-model="newExpense.started"]').datepicker("update", new Date().formatDate("yyyy/MM/dd"));
 
-    $('.dropdown-menu').click(function(e) {
-        e.stopPropagation();
-    });
+    $('.dropdown-menu').click(e => e.stopPropagation());
 
-    $('#frequencySelect').on('hide.bs.dropdown', function() {
-        $scope.$apply(function(){
-            try{
-                switch($scope.frequency.type){
+    $('#frequencySelect').on('hide.bs.dropdown', () => $scope.$apply(function () {
+        try {
+            switch ($scope.frequency.type) {
                 case "basic":
                     $scope.newExpense.frequency = $scope.frequency.date;
                     break;
@@ -180,41 +171,40 @@ app.controller("expensesCtrl", function($scope, $rootScope, $sce) {
                     $scope.newExpense.frequency = "DAY " + $scope.frequency.day + " ";
                     if ($scope.frequency.last) {
                         $scope.newExpense.frequency += "-";
-                    } else if($scope.frequency.date == $scope.getDateMax()) {
+                    } else if ($scope.frequency.date == $scope.getDateMax()) {
                         throw "Invalid date";
                     }
-                    $scope.newExpense.frequency +=  $scope.frequency.date;
+                    $scope.newExpense.frequency += $scope.frequency.date;
                     break;
                 case "monthday":
                     $scope.newExpense.frequency = "DAY ";
                     if ($scope.frequency.last) {
                         $scope.newExpense.frequency += "-";
-                    } else if($scope.frequency.date == $scope.getDateMax()) {
+                    } else if ($scope.frequency.date == $scope.getDateMax()) {
                         throw "Invalid date";
                     }
-                    $scope.newExpense.frequency +=  $scope.frequency.date;
+                    $scope.newExpense.frequency += $scope.frequency.date;
                     break;
                 case "workday":
                     $scope.newExpense.frequency = "WDAY ";
                     if ($scope.frequency.last) {
                         $scope.newExpense.frequency += "-";
-                    } else if($scope.frequency.date == $scope.getDateMax()) {
+                    } else if ($scope.frequency.date == $scope.getDateMax()) {
                         throw "Invalid date";
                     }
-                    $scope.newExpense.frequency +=  $scope.frequency.date;
+                    $scope.newExpense.frequency += $scope.frequency.date;
                     break;
                 case "restday":
                     $scope.newExpense.frequency = "RDAY ";
                     if ($scope.frequency.last) {
                         $scope.newExpense.frequency += "-";
-                    } else if($scope.frequency.date == $scope.getDateMax()) {
+                    } else if ($scope.frequency.date == $scope.getDateMax()) {
                         throw "Invalid date";
                     }
-                    $scope.newExpense.frequency +=  $scope.frequency.date;
+                    $scope.newExpense.frequency += $scope.frequency.date;
                     break;
-                }
-            } catch (e) { $scope.newExpense.frequency = ""; }
-    });
-    });
+            }
+        } catch (e) { $scope.newExpense.frequency = ""; }
+    }));
 
 });
