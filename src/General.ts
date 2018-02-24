817789a470787698d3b56c08103bc9f74470b25d
Muhammed Haque <nohorjo@gmail.com>
2018-02-24 13:59:13 +0000

    #85 Using filesystem to store import progress

--- src/General.ts
+++ src/General.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import * as fs from 'fs';
+import * as uuid from 'uuid/v4';
 
 const route = Router();
 
@@ -8,30 +10,61 @@ route.get("/budget", (req, resp) => {
         afterAuto: 654
     });
 });
-route.get("/rules", (req, resp) => {
-    resp.send(['rule1', 'rule2']);
+
+// =================== IMPORT
+fs.mkdir('importProgress', err => {
+    if (err && err.code != 'EEXIST') throw err;
 });
-route.get("/import", (() => {
-    //FIXME: share between clusters
-    let done = 0;
-    return (req, resp) => {
-        if (done = (++done % 10)) {
-            resp.send({
-                processed: done,
-                total: 10
-            });
+route.get("/rules", (req, resp) => {
+    resp.send([
+        'default',
+        'rule1',
+        'rule2'
+    ]);
+});
+route.get("/import", (req, resp) => {
+    const path = `importProgress/${req.query.id}.json`;
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            resp.status(500).send(err);
         } else {
-            resp.send(null);
+            const progress = JSON.parse(data);
+            if (++progress.processed == progress.total) {
+                fs.unlink(path, err => {
+                    if (err) {
+                        resp.status(500).send(err);
+                    } else {
+                        resp.send(null);
+                    }
+                });
+            } else {
+                fs.writeFile(path, JSON.stringify(progress), err => {
+                    if (err) {
+                        resp.status(500).send(err);
+                    } else {
+                        resp.send(progress);
+                    }
+                });
+            }
         }
-    }
-})());
+    });
+});
 route.post("/import", (req, resp) => {
     console.log(req.query.rule);
     if (!req['files']) {
         resp.status(400).send('No files were uploaded.');
-    }
-    else {
-        resp.send(req['files'].csv.data);
+    } else {
+        const id = uuid();
+        fs.writeFile(`importProgress/${id}.json`, JSON.stringify({
+            processed: 0,
+            total: 10
+        }), err => {
+            if (err) {
+                resp.status(500).send(err);
+            } else {
+                resp.send(id);
+            }
+        });
     }
 });
 
