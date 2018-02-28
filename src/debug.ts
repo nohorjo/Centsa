860248a2ebf9119761c9d39e5065d290c90e0101
Muhammed Haque <nohorjo@gmail.com>
2018-02-28 16:49:40 +0000

    #85 Updated debug

--- src/debug.ts
+++ src/debug.ts
@@ -3,13 +3,23 @@ import { Router } from 'express';
 const route = Router();
 
 route.get('/', (req, resp) => {
+    const data = {
+        body: req.body,
+        cookies: req.cookies,
+        session: req.session,
+        params: req.params,
+        query: req.query
+    };
     resp.send(
-        JSON.stringify(req.body)
-        + JSON.stringify(req.cookies)
-        + JSON.stringify(req.session)
-        + JSON.stringify(req.params)
-        + JSON.stringify(req.query)
-    );
+        `<h1>Server</h1>
+        <pre>${JSON.stringify(data, null, 4)}</pre>
+        <h1>Client</h1>
+        <pre id="client"></pre>
+        <script>
+        document.getElementById('client').innerText = JSON.stringify({
+            cookie : document.cookie
+        },null,4);
+        </script>`);
 });
 
 const _route = Router();
